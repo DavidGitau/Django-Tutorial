bde00930f272b289780a1eff2b68ef65534bedda
DavidGitau <86714248+DavidGitau@users.noreply.github.com>
2023-01-06 16:36:22 +0300

    API params

--- api/views.py
+++ api/views.py
@@ -2,31 +2,36 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import generics
 from rest_framework.response import Response
-from api.serializers import *
+from api.serializers import AuthorSerializer
 from tut.models import Author
+from django.db.models import Q
 
 
 class AuthorDetail(APIView):
-    id = 1
-
-    def get(self, request, slug):
-        self.id = slug
-        dt = Author.objects.filter(id=self.id)
+    def get(self, request, pk):
+        dt = Author.objects.filter(id=pk)
         data = AuthorSerializer(dt, many=True).data
         return Response(data)
 
     def get_queryset(self):
-        return Author.objects.filter(id=self.id)
+        return Author.objects.filter(id=self.kwargs['pk'])
     
 
-class AuthorList(APIView):
-    def get(self, request):
-        dt = Author.objects.all()
-        data = AuthorSerializer(dt, many=True).data
-        return Response(data)
+class AuthorList(generics.ListAPIView):
+    # def get(self, request):
+    #     dt = Author.objects.all()
+    #     data = AuthorSerializer(dt, many=True).data
+    #     return Response(data)
+    serializer_class = AuthorSerializer
 
     def get_queryset(self):
-        return Author.objects.all()
+        queryset = Author.objects.all()
+        name = self.request.query_params.get('name')
+        id = self.request.query_params.get('id')
+        if name and id is not None:
+            # print(name)
+            queryset = queryset.filter(Q(name=name)&Q(id=id))
+        return queryset
 
 
 class CustomList(generics.ListCreateAPIView):
